e6aa7132a12cc73fc32304307e473199cfad9374
root <root@DESKTOP-77RQ0M5.localdomain>
2019-08-05 20:31:28 -0300

    feat: Session Store

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 import validateUserStore from './app/validators/UserStore';
+import validateSessionStore from './app/validators/SessionStore';
 
 const routes = new Router();
 
 routes.post('/users', validateUserStore, UserController.store);
+
+routes.post('/session', validateSessionStore, SessionController.store);
 
 export default routes;
