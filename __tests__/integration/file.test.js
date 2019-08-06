6c9978f04f7be884adc644c8479147c1922e2ce3
root <root@DESKTOP-77RQ0M5.localdomain>
2019-08-05 23:14:49 -0300

    feat: File Store

---
+++ __tests__/integration/file.test.js
@@ -0,0 +1,30 @@
+import request from 'supertest';
+import { resolve } from 'path';
+import fs from 'mz/fs';
+import app from '~/app';
+
+import truncate from '../util/truncate';
+
+describe('File', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should storage a file', async () => {
+    const path = resolve(__dirname, '..', 'assets', 'banner.jpg');
+    const response = await request(app)
+      .post('/files')
+      .attach('file', path);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('path');
+
+    const fileName = response.body.path;
+
+    const destPath = resolve(__dirname, '..', 'tmp', fileName);
+
+    console.log(destPath);
+
+    expect(await fs.exists(destPath)).toBe(true);
+  });
+});
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import validateUserStore from './app/validators/UserStore';
 import validateSessionStore from './app/validators/SessionStore';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', validateUserStore, UserController.store);
 
 routes.post('/session', validateSessionStore, SessionController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
